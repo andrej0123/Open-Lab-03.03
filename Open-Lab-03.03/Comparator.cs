6c33451be4b9e11dccffdd1ba687f6856e8ac0cd
z20ist24 <z20ist24@Z00P1-05>
2021-10-20 08:17:35 +0200

    comparees the length of string

--- Open-Lab-03.03/Comparator.cs
+++ Open-Lab-03.03/Comparator.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_03._03
     {
         public bool CompareCharactersCount(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int length1 = str1.Length;
+            int length2 = str2.Length;
+            
+            if (length1 == length2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
